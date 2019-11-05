34d890094bf4c8fa41edcc5f24602db7e49e4156
magnuss17 <magnuss17@ru.is>
2019-11-05 18:43:48 +0000

    if_else litle update

--- if_else.py
+++ if_else.py
@@ -3,42 +3,6 @@ from ple import PLE
 import random
 
 class IfElseAgent:
-    def __init__(self):
-        # TODO: you may need to do some initialization for your agent here
-        return
-    
-    def reward_values(self):
-        """ returns the reward values used for training
-        
-            Note: These are only the rewards used for training.
-            The rewards used for evaluating the agent will always be
-            1 for passing through each pipe and 0 for all other state
-            transitions.
-        """
-        return {"positive": 1.0, "tick": 0.0, "loss": -5.0}
-    
-    def observe(self, s1, a, r, s2, end):
-        """ this function is called during training on each step of the game where
-            the state transition is going from state s1 with action a to state s2 and
-            yields the reward r. If s2 is a terminal state, end==True, otherwise end==False.
-            
-            Unless a terminal state was reached, two subsequent calls to observe will be for
-            subsequent steps in the same episode. That is, s1 in the second call will be s2
-            from the first call.
-            """
-        # TODO: learn from the observation
-        return
-
-    def training_policy(self, state):
-        """ Returns the index of the action that should be done in state while training the agent.
-            Possible actions in Flappy Bird are 0 (flap the wing) or 1 (do nothing).
-
-            training_policy is called once per frame in the game while training
-        """
-        print("state: %s" % state)
-        # TODO: change this to to policy the agent is supposed to use while training
-        # At the moment we just return an action uniformly at random.
-        return random.randint(0, 1) 
 
     def policy(self, state):
         """ Returns the index of the action that should be done in state when training is completed.
@@ -52,14 +16,23 @@ class IfElseAgent:
         bird_position = state['player_y']
         bird_vel = state['player_vel']
         next_pipe_distance = state['next_pipe_dist_to_player']
-        next_pipe_center = state['next_pipe_bottom_y'] - 55
+        next_pipe_center = state['next_pipe_bottom_y'] - 50
         next_next_pipe_distance = state['next_next_pipe_dist_to_player']
-        next_next_pipe_center =state['next_next_pipe_bottom_y'] - 55
+        next_next_pipe_center =state['next_next_pipe_bottom_y'] - 50
+        action = 0
+        if next_pipe_distance < 10:
+            next_pipe_distance = next_next_pipe_distance
+            next_pipe_center = next_next_pipe_center
 
-        if next_pipe_center > bird_position:
+        if bird_position < next_pipe_center:
             action = 1
+        if next_pipe_distance < 100:
+            if bird_vel < 0:
+                action = 0
         else:
-            action = 0
+            if bird_vel < 0:
+                action = 1
+
         return action
 
 def run_game(nb_episodes, agent):
@@ -71,7 +44,7 @@ def run_game(nb_episodes, agent):
     # TODO: when training use the following instead:
     # reward_values = agent.reward_values
     
-    env = PLE(FlappyBird(), fps=30, display_screen=True, force_fps=False, rng=None,
+    env = PLE(FlappyBird(), fps=30, display_screen=True, force_fps=True, rng=None,
             reward_values = reward_values)
     # TODO: to speed up training change parameters of PLE as follows:
     # display_screen=False, force_fps=True 
